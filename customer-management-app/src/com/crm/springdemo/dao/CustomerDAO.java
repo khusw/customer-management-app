215a84c09382e1b4595b559e4d6440aaae544754
sdy <vel1024@khu.ac.kr>
2021-02-15 18:40:40 +0900

    create customerDAO implementation

---
+++ customer-management-app/src/com/crm/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,9 @@
+package com.crm.springdemo.dao;
+
+import java.util.List;
+
+import com.crm.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	public List<Customer> getCustomers();
+}
---
+++ customer-management-app/src/com/crm/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,34 @@
+package com.crm.springdemo.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.crm.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory; 
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		Session session = sessionFactory.getCurrentSession();
+		
+		Query<Customer> query = session.createQuery("fron Customer", Customer.class);
+		
+		List<Customer> customers = query.getResultList();
+		
+		return customers;
+	}
+
+}
