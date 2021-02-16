2360b888935247dc43b6c746f9717cf7d91dd19f
sdy <vel1024@khu.ac.kr>
2021-02-16 15:32:44 +0900

    create customer service layer

---
+++ customer-management-app/src/com/crm/springdemo/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.crm.springdemo.service;
+
+import java.util.List;
+
+import com.crm.springdemo.entity.Customer;
+
+public interface CustomerService {
+	public List<Customer> getCustomers();
+}
---
+++ customer-management-app/src/com/crm/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.crm.springdemo.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crm.springdemo.dao.CustomerDAO;
+import com.crm.springdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+}
