dde8476cca4eb32a2105931c53df0c1654cd1720
sdy <vel1024@khu.ac.kr>
2021-02-16 15:34:38 +0900

    change customer service to customer DAO

--- customer-management-app/src/com/crm/springdemo/controller/CustomerController.java
+++ customer-management-app/src/com/crm/springdemo/controller/CustomerController.java
@@ -5,22 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.crm.springdemo.dao.CustomerDAO;
 import com.crm.springdemo.entity.Customer;
+import com.crm.springdemo.service.CustomerService;
+
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
-		List<Customer> customers = customerDAO.getCustomers();
+		
+		List<Customer> customers = customerService.getCustomers();
+		
 		model.addAttribute("customers", customers);
+		
 		return "list-customers";
 	}
 }
