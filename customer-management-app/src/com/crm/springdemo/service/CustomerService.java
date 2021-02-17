3d676172acfb4eac0ab9342911496ccd70db6b59
sdy <vel1024@khu.ac.kr>
2021-02-17 16:42:33 +0900

    add some functions in CustomerService

--- customer-management-app/src/com/crm/springdemo/service/CustomerService.java
+++ customer-management-app/src/com/crm/springdemo/service/CustomerService.java
@@ -6,4 +6,14 @@ import com.crm.springdemo.entity.Customer;
 
 public interface CustomerService {
 	public List<Customer> getCustomers();
+	
+	public void saveCustomer(Customer customer);
+
+	public Customer getCustomer(int id);
+
+	public void deleteCustomer(int id);
+
+	public List<Customer> searchCustomers(String searchName);
+
+	public List<Customer> getCustomers(int sortField);
 }
