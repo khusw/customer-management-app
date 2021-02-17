b9c9cd44360a4f5619b8c76ddc097d24752a815a
sdy <vel1024@khu.ac.kr>
2021-02-17 16:43:24 +0900

    update CustomerController

--- customer-management-app/src/com/crm/springdemo/controller/CustomerController.java
+++ customer-management-app/src/com/crm/springdemo/controller/CustomerController.java
@@ -2,14 +2,21 @@ package com.crm.springdemo.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.crm.springdemo.entity.Customer;
 import com.crm.springdemo.service.CustomerService;
+import com.crm.springdemo.util.SortUtils;
 
 
 @Controller
@@ -20,9 +27,61 @@ public class CustomerController {
 	private CustomerService customerService;
 	
 	@GetMapping("/list")
-	public String listCustomers(Model model) {
+	public String listCustomers(Model model, @RequestParam(required=false) String sort) {
 		
-		List<Customer> customers = customerService.getCustomers();
+		List<Customer> customers = null;
+		
+		if (sort != null) {
+			int sortField = Integer.parseInt(sort);
+			customers = customerService.getCustomers(sortField);
+		} else {
+			customers = customerService.getCustomers(SortUtils.LAST_NAME);
+		}
+		
+		model.addAttribute("customers", customers);
+		
+		return "list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		Customer customer = new Customer();
+		
+		model.addAttribute("customer", customer);
+		
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+			return "customer-form";
+		} else {
+			customerService.saveCustomer(customer);
+			
+			return "redirect:/customer/list";
+		}
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int id, Model model) {
+		Customer customer = customerService.getCustomer(id);
+		
+		model.addAttribute("customer", customer);
+		
+		return "customer-form";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int id) {
+		customerService.deleteCustomer(id);
+		return "redirect:/customer/list";
+	}
+	
+	@GetMapping("/search")
+	public String searchCustomers(@RequestParam("searchName") String searchName, Model model) {
+		List<Customer> customers = customerService.searchCustomers(searchName);
 		
 		model.addAttribute("customers", customers);
 		
