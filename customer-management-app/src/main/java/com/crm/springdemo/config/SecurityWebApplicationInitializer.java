cd2aa7c948817e21c4ba1ff03969fef3bd7a016b
sdy <vel1024@khu.ac.kr>
2021-03-15 21:04:18 +0900

    create SecurityWebApplicationInitializer.java

---
+++ customer-management-app/src/main/java/com/crm/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.crm.springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+    // register spring security filter in project
+}
