74bf82f441baa2a39b56527f8f9cee0eb41dda35
sdy <vel1024@khu.ac.kr>
2021-03-15 21:02:06 +0900

    create CRMUser.java

---
+++ customer-management-app/src/main/java/com/crm/springdemo/user/CRMUser.java
@@ -0,0 +1,33 @@
+package com.crm.springdemo.user;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class CRMUser {
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String userName;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String password;
+
+    public CRMUser() {}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
